9fd5972e214f9f88f893e74cf746eb7593b71b27
kzcheng <kevinzifancheng@gmail.com>
2020-01-14 02:53:24 +0000

    Finished work

---
+++ timer1.js
@@ -0,0 +1,9 @@
+const INPUT = [10, 3, 5, 15, 9];
+
+for (let i = 0; i < INPUT.length; i++) {
+  if (isNaN(INPUT[i]) || INPUT[i] <= 0){}
+    setTimeout(() => {
+      process.stdout.write(`*`);
+    }, INPUT[i] * 1000);
+  }
+}
